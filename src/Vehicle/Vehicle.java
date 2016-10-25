5cafa72e23f1d50476e445553db284f5d8930117
eoghanp <eoghanp95@yahoo.ie>
2016-10-25 17:30:05 +0100

    Refactored vehicle class added

--- src/Vehicle/Vehicle.java
+++ src/Vehicle/Vehicle.java
@@ -1,9 +1,37 @@
 package Vehicle;
 
-public class Vehicle {
+public class Vehicle
+{
+	private final String model;
+	private final int seats;
+	private final String specialFeatures;
+	private final String classification;
+	private boolean available;
 
-	public Vehicle() {
-		
+	public Vehicle(String model, int seats, String specialFeatures, String classification, boolean available) {
+		super();
+		this.model = model;
+		this.seats = seats;
+		this.specialFeatures = specialFeatures;
+		this.classification = classification;
+		this.available = available;
+	}
+
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public int getSeats() {
+		return seats;
+	}
+
+	public String getSpecialFeatures() {
+		return specialFeatures;
+	}
+	
+	public String getClassification() {
+		return classification;
 	}
 
 }
