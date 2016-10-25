798c4ffabbb800dce93ed9256771232dcc00d591
eoghanp <eoghanp95@yahoo.ie>
2016-10-25 16:59:25 +0100

    Refactoring of Vehicle hierarchy complete. Subclasses removed.

--- src/Vehicle/Vehicle.java
+++ src/Vehicle/Vehicle.java
@@ -2,61 +2,36 @@ package Vehicle;
 
 public class Vehicle
 {
-	private String model;
-	private int seats;
-	private String specialFeatures;
-	private String storageSpace;
+	private final String model;
+	private final int seats;
+	private final String specialFeatures;
+	private final String classification;
+	private boolean available;
 
-	public Vehicle(String model, int seats, String specialFeatures, String storageSpace) {
+	public Vehicle(String model, int seats, String specialFeatures, String classification, boolean available) {
 		super();
 		this.model = model;
 		this.seats = seats;
 		this.specialFeatures = specialFeatures;
-		this.storageSpace = storageSpace;
+		this.classification = classification;
+		this.available = available;
 	}
 
 	
 	public String getModel() {
 		return model;
 	}
-
-	
-	public void setModel(String model) {
-		this.model = model;
-		
-	}
-
 	
 	public int getSeats() {
 		return seats;
 	}
 
-	
-	public void setSeats(int seats) {
-		this.seats = seats;
-		
-	}
-
-	
 	public String getSpecialFeatures() {
 		return specialFeatures;
 	}
-
 	
-	public void setSpecialFeatures(String specialFeatures) {
-		this.specialFeatures = specialFeatures;
-		
-	}
-
-	
-	public String getStorageSpace() {
-		return storageSpace;
-	}
-
-	
-	public void setStorageSpace(String storageSpace) {
-		this.storageSpace = storageSpace;
-		
+	public String getClassification() {
+		return classification;
 	}
 
 }
